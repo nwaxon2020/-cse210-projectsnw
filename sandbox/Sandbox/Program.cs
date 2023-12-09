f03614518729b4d24ee2213798169bd977f7fdfd
Prince Nwachukwu <princenwachukwu308@yahoo.com>
2023-12-09 16:23:56 +0100

    progress towards Project for cse210

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -9,8 +9,8 @@ class Program
 
         Video video1 = new Video("Legend Of The Seeker","Mike Williams",44.8);
         Comment vid1Comment1 = new Comment("Mike Johnson","Wow! what a great movie");
-        Comment vid1Comment2 = new Comment("Prince Nwachukwu","I am not a fan of this type of Film but it great");
-        Comment vid1Comment3 = new Comment("Mike Johnson","I don't like this movie");
+        Comment vid1Comment2 = new Comment("Prince Nwachukwu","I am not a fan of this type of Film but it's great");
+        Comment vid1Comment3 = new Comment("Sussan Cowel","I don't like this movie");
         video1.UpdateComment(vid1Comment1);
         video1.UpdateComment(vid1Comment2);
         video1.UpdateComment(vid1Comment3);
@@ -18,7 +18,7 @@ class Program
 
         Video video2 = new Video("Spider-Man","Disney",60.3);
         Comment vid2Comment1 = new Comment("Ikechukwu","No comments");
-        Comment vid2Comment2 = new Comment("Alma","Is it on sale already");
+        Comment vid2Comment2 = new Comment("Alma","Is it on sale already?");
         Comment vid2Comment3 = new Comment("Marry Allison","The production of this movie is poor");
         video2.UpdateComment(vid2Comment1);
         video2.UpdateComment(vid2Comment2);
@@ -28,7 +28,7 @@ class Program
         Video video3 = new Video("Captain America","Marvels",56);
         Comment vid3Comment1 = new Comment("Matthew","Nice job by this team");
         Comment vid3Comment2 = new Comment("Eddie","I Love the ending");
-        Comment vid3Comment3 = new Comment("Johnathan","Fantactis.\n Please when is the prt 2 comming out ");
+        Comment vid3Comment3 = new Comment("Johnathan","Fantactis.\n Please when is the prt 2 comming out");
         Comment vid3Comment4 = new Comment("Rosline Efe","I wish i had super powers");
         video3.UpdateComment(vid3Comment1);
         video3.UpdateComment(vid3Comment2);
--- sandbox/Sandbox/Video.cs
+++ sandbox/Sandbox/Video.cs
@@ -7,6 +7,8 @@ public class Video
     private string _author;
     private double _duration;
 
+    private int numbOfComment;
+
     List<Comment>_comment = new List<Comment>();
 
     public Video(string title, string author, double time)
@@ -14,6 +16,8 @@ public class Video
         _title = title;
         _author = author;
         _duration = time;
+
+        numbOfComment = 0;
     }
 
     public void UpdateComment(Comment comment)
@@ -23,11 +27,10 @@ public class Video
 
     public void DisplayVideoDetails()
     {
-        int numbOfComment = 0;
-
         Console.WriteLine("Welcome to the 'ABSTRACT' Program");
-        Console.WriteLine($"  Title: {_title}\n  Author: {_author},\n  Video-Duration: {_duration}");
+        Console.WriteLine($"  Title: {_title}\n  Author: {_author},\n  Video-Duration: {_duration} mins");
 
+        Console.WriteLine("_____Video Comments_____");
         foreach(Comment comt in _comment)
         {
            Console.WriteLine($"{comt.DisplayComment()}");
