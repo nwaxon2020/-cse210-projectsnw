ca8322494bdc2b3be9353b6ad8495feb6b34eff5
Prince Nwachukwu <princenwachukwu308@yahoo.com>
2023-12-09 17:14:17 +0100

    Abstract completed

--- sandbox/Sandbox/Comment.cs
+++ sandbox/Sandbox/Comment.cs
@@ -2,16 +2,17 @@ using System;
 
 public class Comment
 {
-    private string _commenterName;
-    private string _text;
+    private string _commenterName; // CommnerName Attribute
+    private string _text; // Comment Attribute
 
+    // Contructor to pass in the Commenter Name and the Comment
     public Comment(string comment, string text)
     {
         _commenterName = comment;
         _text = text;
     }
 
-    public string DisplayComment()
+    public string DisplayComment() // Return the Name of the commenter and the comment
     {
         return $"Name: {_commenterName}\nComment: --{_text}--";
     }
